1a44f44def9a5f492516348787ae011c1a35b3fa
Andrii Borovyi <wanderlust@ukr.net>
2018-06-29 00:30:18 +0300

    Add Django REST framework to make a dynamic API

--- orden_site/news/models.py
+++ orden_site/news/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class News(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
-    content = models.CharField()
+    content = models.TextField()
     pub_date = models.DateTimeField('published')
--- orden_site/orden_site/urls.py
+++ orden_site/orden_site/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .routers import router
 
 urlpatterns = [
     path('news/', include('news.urls')),
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
 ]
