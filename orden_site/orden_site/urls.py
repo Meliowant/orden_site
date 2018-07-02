57e998ef2881b4f40c16ef9d8c83366b2ba751a1
Andrii Borovyi <wanderlust@ukr.net>
2018-07-02 17:20:34 +0300

    Add vue.js UI logic and update URLs to display the page.

--- orden_site/orden_site/urls.py
+++ orden_site/orden_site/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .routers import router
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('news/', include('news.urls')),
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('vnews', TemplateView.as_view(template_name='index_vue.html')),
 ]
