f6171f172d82081d505c94bec58629a336bd996c
tarasoh <mtarasoh@gmail.com>
2018-09-15 18:09:35 +0300

    Added view 'add' for adding news
    Added view 'add' for adding news

--- orden_site/news/views.py
+++ orden_site/news/views.py
@@ -16,3 +16,21 @@ def index(request):
 def detail(request, new_id):
     news_list = News.objects.get(pk=new_id) #.order_by('-id')#[:5]
     return HttpResponse("You're looking at new: %s." % news_list.content)
+
+def add(request):
+    if request.method == 'GET':
+        form = AddNew()
+        return render(request, 'news/add.html', {'form': form})
+    elif request.method == 'POST':
+        form = AddNew(request.POST, request.FILES)
+        if form.is_valid():
+            new = News.objects.create(title=form.cleaned_data['title'], content=form.cleaned_data['content'], pub_date = timezone.now())
+            new.save()
+            for f in request.FILES.getlist('photos'):
+                data = f.read() #Если файл целиком умещается в памяти
+                photo = Photo(new=new)
+                photo.image.save(f.name, ContentFile(data))
+                photo.save()
+            return redirect('')
+        else:
+            return render(request, 'news/add.html', {'form': form})
