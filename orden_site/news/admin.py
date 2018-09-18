48b9686f2afe863697429222ccf060c26898ea80
tarasoh <mtarasoh@gmail.com>
2018-09-18 20:12:52 +0300

    try to add botton "delete img" from new in admin site

--- orden_site/news/admin.py
+++ orden_site/news/admin.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 
 from .models import News, Photo
 
-class PhotoInline(admin.StackedInline):
+#class PhotoInline(admin.StackedInline):
+class PhotoInline(admin.TabularInline):
     model = Photo
+#    fields = ('image_tag',)
+    readonly_fields = ('image_tag','delete_im')
     extra = 1
+    delete = 'true'
 
 class NewsAdmin(admin.ModelAdmin):
     fields = ['title', 'content', 'pub_date']
--- orden_site/news/models.py
+++ orden_site/news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.html import format_html
 
 class News(models.Model):
     id = models.AutoField(primary_key=True)
@@ -10,7 +11,14 @@ class News(models.Model):
         
 class Photo(models.Model):
     id = models.AutoField(primary_key=True)
-    image = models.ImageField(upload_to='news/static/news/photo/')
+    image = models.ImageField(upload_to='photo/')
     new = models.ForeignKey(News, related_name='photos', on_delete=models.CASCADE)
-    def get_url(self):
-    	return self.image.image
+	
+    def __str__(self):
+        return format_html('<a src="{}"/>'.format(self.image.url))
+
+    def image_tag(self):
+	    return format_html('<img src="{}" width="100" height="100" />'.format(self.image.url))
+
+    def delete_im(self):
+        return format_html('<form method="POST" action="" enctype="multipart/form-data"><input type="submit" class="btn btn-default" value="Видалити"/></form>')
