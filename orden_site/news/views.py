52a9ff2acdae36d20bba44ea98ab53defb23e035
tarasoh <mtarasoh@gmail.com>
2018-09-18 20:16:15 +0300

    updated function "detail"

--- orden_site/news/views.py
+++ orden_site/news/views.py
@@ -9,23 +9,17 @@ from django.core.files.base import ContentFile
 
 # Create your views here.
 def index(request):
-    latest_news_list = News.objects.order_by('-pub_date')#[:5]
+    latest_news_list = News.objects.order_by('-pub_date')
     template = loader.get_template('news/index.html')
     context = {
         'latest_news_list': latest_news_list,
     }
     return HttpResponse(template.render(context, request))
 
-#def detail(request, new_id):
-#    news_list = News.objects.get(pk=new_id) #.order_by('-id')#[:5]
-#    return HttpResponse("You're looking at new: %s." % news_list.content)
-
 def detail(request, new_id):
     new = News.objects.get(pk=new_id)
-#    p_list={}
-#    p_list.extend(Photo.objects.get()) 
     p_list = Photo.objects.filter(new=new)
-    template = loader.get_template('news/detail_1.html')
+    template = loader.get_template('news/detail.html')
     context = {'new': new,'p_list': p_list}
     return HttpResponse(template.render(context, request))
 
@@ -40,11 +34,11 @@ def add(request):
                 new = News.objects.create(title=form.cleaned_data['title'], content=form.cleaned_data['content'], pub_date = timezone.now())
                 new.save()
                 for f in request.FILES.getlist('photos'):
-                    data = f.read() #Если файл целиком умещается в памяти
+                    data = f.read()
                     photo = Photo(new=new)
                     photo.image.save(f.name, ContentFile(data))
                     photo.save()
-                return redirect('new/')
+                return redirect('/news/%d/'%new.id)
         else:
             return render(request, 'news/add.html', {'form': form})
     else: 
